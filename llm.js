452f1392699c42e79036b27b4d4e1b7581071b39
praneeth <gp.gunturu@gmail.com>
2026-01-23 17:45:46 -0600

    gpt model through cloud api (ollama)

--- llm.js
+++ llm.js
@@ -1,8 +1,28 @@
-import ollama from 'ollama'
+// import ollama from 'ollama'
 
-const response = await ollama.chat({
-  model: 'gemma3',
-  messages: [{ role: 'user', content: 'Hello' }],
+// const response = await ollama.chat({
+//   model: 'gemma3',
+//   messages: [{ role: 'user', content: 'Hello' }],
+// })
+
+// console.log(response)
+
+import dotenv from 'dotenv'
+dotenv.config()
+import { Ollama } from 'ollama'
+
+const ollama = new Ollama({
+    host: 'https://ollama.com',
+    headers: { Authorization: 'Bearer ' + process.env.OLLAMA_API_KEY },
 })
 
-console.log(response)
+const response = await ollama.chat({
+    model: 'gpt-oss:120b',
+    messages: [{ role: 'user', content: 'Hello' }],
+})
+
+console.log(response);
+
+// for await (const part of response) {
+//     process.stdout.write(part.message.content)
+// }
